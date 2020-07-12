a023845f2bed1380995e8bb78d029ae95897bb12
unknown <shamolmojumder14@gmail.com>
2020-07-12 13:26:07 +0600

    Update two fields at the same time, create function

--- do.js
+++ do.js
@@ -1,6 +1,17 @@
-const currentSelleingPrice = 199;
-const sellingPrice = 799;
+// const currentSelleingPrice = 199;
+// const sellingPrice = 799;
 
-const discountPercentce = (((sellingPrice - currentSelleingPrice) / sellingPrice) * 100)
-const roundDiscountPercentce = Math.round(discountPercentce);
-console.log("current disount is ", roundDiscountPercentce, "% off");
+// const discountPercentce = (((sellingPrice - currentSelleingPrice) / sellingPrice) * 100)
+// const roundDiscountPercentce = Math.round(discountPercentce);
+// console.log("current disount is ", roundDiscountPercentce, "% off");
+
+const names = ["shamol", "dipu", "mojumder"]
+
+// const nameUpperCase = names.map(function(name) {
+//     return name.toUpperCase()
+// })
+// console.log(nameUpperCase);
+for (let i = 0; i < names.length; i++) {
+    let element = names[i].topUpperCase();
+    console.log(element);
+}
--- script.js
+++ script.js
@@ -12,17 +12,28 @@ depositBtn.addEventListener("click", function() {
     const depositNumber = parseFloat(depositAmount)
 
 
-    const currentDeposit = document.getElementById("currentDeposit").innerText;
-    const currentDepositNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumber;
-    document.getElementById("currentDeposit").innerText = totalDeposit
+    // const currentDeposit = document.getElementById("currentDeposit").innerText;
+    // const currentDepositNumber = parseFloat(currentDeposit);
+    // const totalDeposit = depositNumber + currentDepositNumber;
+    // document.getElementById("currentDeposit").innerText = totalDeposit
+    // current-balance
+    // const currentBalance = document.getElementById("currentBalance").innerText;
+    // const currentBalanceNumber = parseFloat(currentBalance);
+    // const totalBalance = currentBalanceNumber + depositNumber;
+    // document.getElementById("currentBalance").innerText = totalBalance;
     document.getElementById("depositAmount").value = " "
-        // if (currentDepositNumber > 0) {
-        //     alert("not possiable")
-        // } else {
+    updateSpanText("currentDeposit", depositNumber)
+    updateSpanText("currentBalance", depositNumber)
 
-
-    // }
-
-
+    function updateSpanText(id, depositNumber) {
+        const currentAmount = document.getElementById(id).innerText;
+        const currentNumber = parseFloat(currentAmount);
+        const totalAmount = currentNumber + depositNumber
+        document.getElementById(id).innerText = totalAmount;
+    }
+    // const currentBalance = document.getElementById("currentbalance").innerText;
+    // const currentBalanceNumber = parseFloat(currentBalance)
+    // const totalBalance = currentBalanceNumber + currentDepositNumber;
+    // document.getElementById("current-balance").innerText = totalBalance;
+    // document.getElementById("depositAmount").value = " "
 })
